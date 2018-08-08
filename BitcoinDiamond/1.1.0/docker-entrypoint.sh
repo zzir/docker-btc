4bb4194b6089e0519c3b50600c360d00b78f87f2
zzir <zzir@live.cn>
2018-08-08 14:01:57 +0800

    first commit

---
+++ BitcoinDiamond/1.1.0/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for bitcoindiamondd"
+
+  set -- bitcoindiamondd "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoindiamondd" ]; then
+  mkdir -p "$BITCOIN_DIAMOND_DATA"
+  chmod 700 "$BITCOIN_DIAMOND_DATA"
+  chown -R bitcoin "$BITCOIN_DIAMOND_DATA"
+
+  echo "$0: setting data directory to $BITCOIN_DIAMOND_DATA"
+
+  set -- "$@" -datadir="$BITCOIN_DIAMOND_DATA"
+fi
+
+if [ "$1" = "bitcoindiamondd" ] || [ "$1" = "bitcoindiamond-cli" ] || [ "$1" = "bitcoindiamond-tx" ]; then
+  echo
+  exec gosu bitcoindiamond "$@"
+fi
+
+echo
+exec "$@"
